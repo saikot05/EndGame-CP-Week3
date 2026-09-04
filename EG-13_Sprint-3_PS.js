ac5ad155b6fb97bb464af3097e32eaea0ff38a31
saikot05 <saikotislam08@gmail.com>
2026-09-04 22:47:37 +0600

    Solve top k frequent elements

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -105,3 +105,17 @@ var subarraySum = function(nums, k) {
 
     return result;
 };
+//08. Top K Frequent Elements
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const count = {};
+    for (const num of nums) {
+        count[num] = (count[num] || 0) + 1;
+    }
+    const sorted = Object.keys(count).sort((a, b) => count[b] - count[a]);
+    return sorted.slice(0, k).map(Number);
+};
