27a96c8ef0a5485ec6ac3544d6fcaa47dad8093e
saikot05 <saikotislam08@gmail.com>
2026-09-04 21:42:34 +0600

    Solve Moves zeroes problem

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -6,4 +6,19 @@
 var containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length;
 };
-console.log(containsDuplicate([1, 2, 3, 1])); // true
+//02. Move Zeroes
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            j++;
+        }
+    }
+    return nums;
+
+};
