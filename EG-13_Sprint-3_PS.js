4b29a7f40204932a77abd971abf2853eaaf1f978
saikot05 <saikotislam08@gmail.com>
2026-09-04 22:06:27 +0600

    Solve Majority Element

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -50,3 +50,15 @@ var canConstruct = function(ransomNote, magazine) {
     }
     return true;
 };
+//05. Majority Element
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const count = {};
+    for (const num of nums) {
+        count[num] = (count[num] || 0) + 1;
+        if (count[num] > nums.length / 2) return num;
+    }
+};
