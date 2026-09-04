970632caeddd242b2d1ca037a94b9c669780da5d
saikot05 <saikotislam08@gmail.com>
2026-09-04 22:52:17 +0600

    Solve longest sequence

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -119,3 +119,29 @@ var topKFrequent = function(nums, k) {
     const sorted = Object.keys(count).sort((a, b) => count[b] - count[a]);
     return sorted.slice(0, k).map(Number);
 };
+//09. Longest Consecutive Sequence
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if (nums.length === 0) return 0;
+    const numSet = new Set(nums);
+    let longestStreak = 0;
+
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentStreak = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum++;
+                currentStreak++;
+            }
+
+            longestStreak = Math.max(longestStreak, currentStreak);
+        }
+    }
+
+    return longestStreak;
+};
