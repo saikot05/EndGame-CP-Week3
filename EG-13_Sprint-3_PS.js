96aee34e4c7088950c3ce1786c5891293166e112
saikot05 <saikotislam08@gmail.com>
2026-09-04 22:54:59 +0600

    Solve Sort Colors

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -145,3 +145,27 @@ var longestConsecutive = function(nums) {
 
     return longestStreak;
 };
+//10. Sort Colors
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let low = 0;
+    let mid = 0;
+    let high = nums.length - 1;
+
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[low], nums[mid]] = [nums[mid], nums[low]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 2) {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
+        } else {
+            mid++;
+        }
+    }
+
+};
